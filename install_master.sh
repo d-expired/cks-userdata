3a2a8afdb44142441798ba6e9a8ac2b28e680d0e
dean-alphafx <65293263+dean-alphafx@users.noreply.github.com>
2021-04-03 22:19:02 +0100

    Update install_master.sh

--- install_master.sh
+++ install_master.sh
@@ -58,3 +58,10 @@ kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumC
 echo
 echo "### COMMAND TO ADD A WORKER NODE ###"
 kubeadm token create --print-join-command --ttl 0
+
+echo
+echo "Install Weave CNI"
+mkdir -p ~/.kube
+cp -i /etc/kubernetes/admin.conf ~/.kube/config
+kubectl version --short
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=`kubectl version | base64 | tr -d '\n'`"
