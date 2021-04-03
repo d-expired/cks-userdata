fe2b17bb81e7355d81e6f9a3fc3eabf2993256d2
dean-alphafx <65293263+dean-alphafx@users.noreply.github.com>
2021-04-03 20:38:26 +0100

    Update install_master.sh

--- install_master.sh
+++ install_master.sh
@@ -58,7 +58,7 @@ kubeadm reset -f
 kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumCPU --skip-token-print
 
 mkdir -p ~/.kube
-sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+cp -i /etc/kubernetes/admin.conf ~/.kube/config
 
 sleep 5
 
