884293307db65c2d16d5020123d2d0fa9f4d92a8
dean-alphafx <65293263+dean-alphafx@users.noreply.github.com>
2021-04-03 21:44:46 +0100

    Update install_master.sh

--- install_master.sh
+++ install_master.sh
@@ -11,8 +11,8 @@ echo 'set shiftwidth=2' >> ~/.vimrc
 echo 'set expandtab' >> ~/.vimrc
 echo 'source <(kubectl completion bash)' >> ~/.bashrc
 echo 'alias k=kubectl' >> ~/.bashrc
-echo 'alias kgp=k get pods' >> ~/.bashrc
-echo 'alias kgpw=watch kubectl get pods' >> ~/.bashrc
+echo 'alias kgp="k get pods"' >> ~/.bashrc
+echo 'alias kgpw="watch kubectl get pods"' >> ~/.bashrc
 echo 'alias c=clear' >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
